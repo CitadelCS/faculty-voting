c0cb72a6d673e0a2ad99286a5bc658a93688c127
Charles Martin <cmarti14@citadel.edu>
2020-11-09 02:36:59 +0000

    Fixing Fenny's Mess

--- db/migrate/20201021191450_create_proposals.rb
+++ db/migrate/20201021191450_create_proposals.rb
@@ -3,7 +3,7 @@ class CreateProposals < ActiveRecord::Migration[6.0]
     create_table :proposals do |t|
       t.string :title
       t.text :text
-
+      t.integer :active, default: 0  
       t.timestamps
     end
   end
